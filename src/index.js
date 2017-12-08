b82627b731bb514de0e7ebef5d52efe0c282b40a
zhanbohui <919250680@qq.com>
2017-12-08 15:16:03 +0800

    test for v0.0.1
    测试分支用

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: jammy 
+ * @Date: 2017-12-08 15:14:15 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2017-12-08 15:15:28
+ */
+
+function test() {
+    console.log('test for 分支');
+}
